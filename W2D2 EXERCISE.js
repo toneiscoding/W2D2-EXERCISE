abfcd6758d3203dd1b5d649216664f96c0abf3ef
Anthoni Duong <aduong@LABA.PSWAS.LOCAL>
2023-04-04 10:17:38 -0700

    lets hope this works

---
+++ W2D2 EXERCISE.js
@@ -0,0 +1,28 @@
+//1
+function isEven(num) {
+    return num % 2 === 0;
+  }
+  
+
+//2 
+function sayHello() {
+    console.log("Hello");
+  }
+  
+//3
+function convertToFahrenheit(celsius) {
+    return celsius * 9/5 + 32;
+  }
+  
+//4 
+function saySomething() {
+    console.log("Still not as good as I want to be");
+  }
+  
+  
+
+//5
+function practiceMore() {
+    console.log("I need to practice more and more");
+  }
+  
---
+++ index.js
@@ -0,0 +1,2 @@
+// Sup Chase this is my Javascript file, do you like it?
+ol.mylist 
